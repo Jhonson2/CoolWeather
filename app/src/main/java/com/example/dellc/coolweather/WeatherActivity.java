92f4effdf5d5694160e139ad231c92b0570ea44f
Jhonson2 <1750896420@qq.com>
2017-06-15 09:15:52 +0800

    获取必应每天一图：Glide加载+背景图与状态图融合

--- app/src/main/java/com/example/dellc/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/dellc/coolweather/WeatherActivity.java
@@ -1,19 +1,20 @@
 package com.example.dellc.coolweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
-import android.support.v4.view.ScrollingView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.dellc.coolweather.gson.Forecast;
 import com.example.dellc.coolweather.gson.Weather;
 import com.example.dellc.coolweather.util.HttpUtil;
@@ -44,36 +45,57 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
     private TextView sportText;
 
+    private ImageView bingPicimg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >=21){
+            View decorView=getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
 
         //初始化各控件
-        weatherLayout= (ScrollView) findViewById(R.id.weather_layout);
-        titleCity= (TextView) findViewById(R.id.title_city);
-        titleUpdateTime= (TextView) findViewById(R.id.title_update_time);
-        degreeText= (TextView) findViewById(R.id.degree_text);
-        weatherInfoText= (TextView) findViewById(R.id.weather_info_text);
+        bingPicimg = (ImageView) findViewById(R.id.bing_pic_img);
 
-        forecastLayout= (LinearLayout) findViewById(R.id.forecast_layout);
-        aqiText= (TextView) findViewById(R.id.aqi_text);
-        pm25Text= (TextView) findViewById(R.id.pm25_text);
-        comfortText= (TextView) findViewById(R.id.comfort_text);
-        carWashText= (TextView) findViewById(R.id.car_wash_text);
-        sportText= (TextView) findViewById(R.id.sport_text);
 
-        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
-        String weatherString =prefs.getString("weather",null);
+        weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
+        titleCity = (TextView) findViewById(R.id.title_city);
+        titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
+        degreeText = (TextView) findViewById(R.id.degree_text);
+        weatherInfoText = (TextView) findViewById(R.id.weather_info_text);
 
-        if(weatherString !=null){
+        forecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
+        aqiText = (TextView) findViewById(R.id.aqi_text);
+        pm25Text = (TextView) findViewById(R.id.pm25_text);
+        comfortText = (TextView) findViewById(R.id.comfort_text);
+        carWashText = (TextView) findViewById(R.id.car_wash_text);
+        sportText = (TextView) findViewById(R.id.sport_text);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather", null);
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicimg);
+
+        } else {
+            loadBingPic();
+        }
+
+        if (weatherString != null) {
             //有缓存时候直接解析天气数据
-            Weather weather= Utility.handleWeatherResponse(weatherString);
+            Weather weather = Utility.handleWeatherResponse(weatherString);
             showWeatherInfo(weather);
-        }else{
+        } else {
             //没有缓存时候去服务器查询天气
-            String weatherId=getIntent().getStringExtra("weather_id");
+            String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
         }
@@ -122,6 +144,38 @@ public class WeatherActivity extends AppCompatActivity {
                     }
                 });
 
+            }
+        });
+        loadBingPic();
+    }
+
+
+    /*
+        * *jia zai BingPic Img
+        */
+    private void loadBingPic() {
+        String requestBingPic="http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic=response.body().string();
+                SharedPreferences.Editor editor=PreferenceManager
+                        .getDefaultSharedPreferences(WeatherActivity.this).edit();
+
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicimg);
+                    }
+                });
             }
         });
     }
